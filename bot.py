402856aafc94103a92cc6170fb666cce1e3c2cfc
chameeralchathuranga-beep <chameeralchathuranga@gmail.com>
2025-10-18 21:19:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = "8485070607:AAEo3OupUbB34E6POtfFhC1nBIGzcfBq1h0"
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # ✅ Load from Railway Variables
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("DAKSHINA LANKA BELIATHTHA 🚀")
